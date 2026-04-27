4ba73a8ca6aff903972b59c4794f138fc8017213
Ítalo Santana <italoeugenio539@gmail.com>
2026-04-27 18:08:53 -0300

    Commit inicial setup

---
+++ src/test/java/com/italosantana/livros_api/LivrosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.italosantana.livros_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivrosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
