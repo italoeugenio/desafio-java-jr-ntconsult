027d0a31cb0a87e1911e73066eb5a9dfbb6463ae
Ítalo Santana <italoeugenio539@gmail.com>
2026-04-29 07:31:53 -0300

    refactor: adiciona mapeamento explicito de colunas em snake_case

--- src/main/java/com/italosantana/livros_api/domain/entities/LivroModel.java
+++ src/main/java/com/italosantana/livros_api/domain/entities/LivroModel.java
@@ -16,10 +16,21 @@ public class LivroModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+
+    @Column(name = "titulo")
     private String titulo;
+
+    @Column(name = "autor")
     private String autor;
+
+    @Column(name = "ano_publicacao")
     private Integer anoPublicacao;
+
+    @Column(name = "criado_em")
     private LocalDateTime criadoEm = LocalDateTime.now();
+
+    @Column(name = "atualizado_em")
     private LocalDateTime atualizadoEm = LocalDateTime.now();
 }
